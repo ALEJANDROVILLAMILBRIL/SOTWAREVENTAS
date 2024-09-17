2849618c575ad89b21cac4b0ecc0b8e32c667007
ALEJANDROVILLAMILBRIL <nelsonvillamil222@unisangil.edu.co>
2024-09-17 15:00:48 -0500

    Agregar cambio entre ventanas y ventana home

--- app/src/main/java/com/example/softwareventas/MainActivity.java
+++ app/src/main/java/com/example/softwareventas/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.softwareventas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.softwareventas.activitys.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView registerPrompt = findViewById(R.id.registerPrompt);
+        registerPrompt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/softwareventas/activitys/RegisterActivity.java
+++ app/src/main/java/com/example/softwareventas/activitys/RegisterActivity.java
@@ -2,8 +2,12 @@ package com.example.softwareventas.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.softwareventas.MainActivity;
 import com.example.softwareventas.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,5 +16,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Texto de inicio de sesión
+        ImageButton loginPrompt = findViewById(R.id.backButton);
+        loginPrompt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
